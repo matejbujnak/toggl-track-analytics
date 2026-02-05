78324d1d275182b8a855c2182c3cfd1733121744
Matej Bujňák <bujnama3@fit.cvut.cz>
2026-02-05 10:23:09 +0100

    cleaned

---
+++ src/components/Navbar.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <nav class="navbar">
+    <div class="container">
+      <RouterLink to="/" class="brand">Toggl Track Analytics</RouterLink>
+      <div class="links">
+        <RouterLink to="/" active-class="active">Home</RouterLink>
+        <RouterLink to="/about" active-class="active">About</RouterLink>
+      </div>
+    </div>
+  </nav>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,8 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+<script setup lang='ts'>
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1>Toggl Track Analytics</h1>
   </main>
 </template>
